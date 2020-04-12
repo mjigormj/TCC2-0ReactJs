74c75ce7df9d64fb8474532f0ae5d72a66defff8
Igor Cruz <47676774+mjigormj@users.noreply.github.com>
2020-04-12 05:30:32 -0300

    icons

--- tcc/src/components/Acao.js
+++ tcc/src/components/Acao.js
@@ -2,36 +2,42 @@ import React from 'react';
 import "../css/acao.css";
 import cadIcon from "../img/cad.png";
 import editIcon from "../img/edit.png";
+import histIcon from "../img/hist.png";
+import entrIcon from "../img/entr.png";
+import msgIcon from "../img/msg.png";
 
 const Acao = () => (
     <div>
         <section className="container">
-            
+
             <div className="opcao">
-                <img src={cadIcon} alt="cadIcon"/>
+                <img src={cadIcon} alt="cadIcon" />
                 <p>Cadastrar</p>
             </div>
 
             <div className="opcao">
-                <img src={editIcon} alt="editIcon"/>
+                <img src={editIcon} alt="editIcon" />
                 <p>Editar</p>
             </div>
 
             <div className="opcao">
-                <img src={cadIcon} alt="cadIcon"/>
-                    
-            </div>        
-
-            <div className="opcao">
-                <img src={cadIcon} alt="cadIcon"/>
-            </div>
-            
-            <div className="opcao">
-                <img src={cadIcon} alt="cadIcon"/>
+                <img src={histIcon} alt="histIcon" />
+                <p>Historico</p>
             </div>
 
             <div className="opcao">
-                <img src={cadIcon} alt="cadIcon"/>
+                <img src={entrIcon} alt="entrIcon" />
+                <p>Entrega</p>
+            </div>
+
+            <div className="opcao">
+                <img src={msgIcon} alt="msgIcon" />
+                <p>Caixa de Mensagens</p>
+            </div>
+
+            <div className="opcao">
+                <img src={cadIcon} alt="cadIcon" />
+                <p></p>
             </div>
         </section>
     </div>
