831af066090abbaa6e38eb89e77a874bb57e10fb
Pedro Lima <pedro.l2002@hotmail.com>
2020-07-13 23:39:16 -0300

    Login sincronizado com a API

--- tcc/src/pages/Login.js
+++ tcc/src/pages/Login.js
@@ -1,40 +1,64 @@
 import React from 'react';
 import "../css/login.css";
 import "../css/global.css";
+import axios from 'axios';
 
-const Login = () => (
-    //function App() {
-    //return (
 
-    <div id="app">
-        <div className="modalLogin">
-            <div id="login">
-                <strong className="titulo">Login</strong>
-                <form>
-                    <div className="input-block">
-                        <label htmlFor="usuario">Usuário</label>
-                        <input name="usuario" id="usuario" required />
-                    </div>
 
-                    <div className="input-block">
-                        <label htmlFor="senha">Senha</label>
-                        <input type="password" id="senha" required />
-                    </div>
+export default class Login extends React.Component{
+    
+    state = {
+        usuario:'',
+        senha:'',
+    };
 
-                    <div id="btn-container" >
-                        <button id="entrar" type="submit"> <a href="/Home"> Entrar</a></button>
-                    </div>
+    handleChange = event =>{
+        this.setState({usuario: event.target.value, senha: event.target.value})
+    }
 
-                </form>
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const conta = {
+            usuario: this.state.usuario,
+            senha: this.state.senha
+        };
+
+        axios.post(`http://127.0.0.1:5000/login`, {usuario: conta.usuario, senha: conta.senha})
+            .then(res=>{
+                console.log(res);
+                console.log(res.output)
+            })
+    };
+    
+    render(){
+        return (
+        <div id="app">
+            <div className="modalLogin">
+                <div id="login">
+                    <strong className="titulo">Login</strong>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="input-block">
+                            <label htmlFor="usuario">Usuário</label>
+                            <input name="usuario" id="usuario" required onChange={this.handleChange}/>
+                        </div>
+
+                        <div className="input-block">
+                            <label htmlFor="senha">Senha</label>
+                            <input type="password" id="senha" required onChange={this.handleChange}/>
+                        </div>
+
+                        <div id="btn-container" >
+                            <button id="entrar" type="submit"> <a href="/Home"> Entrar</a></button>
+                        </div>
+
+                    </form>
+                </div>
             </div>
+            <script src="btn.js"></script>
         </div>
-        <script src="btn.js"></script>
+        )
+    }
+    
 
-
-    </div>
-
-
-);
-//}
-
-export default Login;
+}
